2d70786cb03fb920b00ac992b9d0d1f41c14ccd6
Michael Sky <electrydev@gmail.com>
2017-05-06 22:04:33 +0200

    grouper: Cleanup and merge BoardConfig.mk
    * Merge BoardConfig.mk and BoardConfigCommon.mk,
       there is no point in having them separate...
       tilapia can just include BoardConfig.mk
    * Update copyright info
    * Group and sort flags
    * Remove USE_CLANG_PLATFORM_BUILD, it is true by default on 14.1
    
    Change-Id: Ie81a194c44fe3b39fa55b7ed2b16bf50a9dc2129
    Signed-off-by: Michael Sky <electrydev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,6 @@
+#
 # Copyright (C) 2010 The Android Open Source Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +13,81 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+LOCAL_PATH := device/asus/grouper
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := grouper
+
+# Platform
+TARGET_BOARD_PLATFORM := tegra3
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) \
+			androidboot.selinux=permissive
+TARGET_KERNEL_SOURCE := kernel/asus/grouper
+TARGET_KERNEL_CONFIG := grouper_defconfig
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Dump State
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.grouper
+
+# Filesystem
+BOARD_FLASH_BLOCK_SIZE             := 4096
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 464519168
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 681574400
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 6567231488
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Disable journaling on system.img to save space
+BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
+
+# Only pre-optimize the boot image
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+
+# Configure jemalloc for low-memory
+MALLOC_SVELTE := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.grouper
+
+# TWRP Support - Optional
+ifeq ($(WITH_TWRP),true)
+-include $(LOCAL_PATH)/twrp.mk
+endif
+
+# Security
+BOARD_USES_SECURE_SERVICES := true
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+
+# Wi-Fi
+BOARD_WLAN_DEVICE           := bcmdhd
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
 -include vendor/asus/grouper/BoardConfigVendor.mk
-include device/asus/grouper/BoardConfigCommon.mk
-
-TARGET_RECOVERY_FSTAB := device/asus/grouper/rootdir/fstab.grouper
